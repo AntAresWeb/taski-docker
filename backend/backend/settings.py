44019a375d716d2ee40115e6a4c3e6c99472c246
Андрей Давыдов (Andrey Davydov) <davy.anre@yandex.ru>
2023-05-22 20:29:04 +0300

    Add workflow with PEP8 linting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,8 +1,10 @@
+from dotenv import load_dotenv
 import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 DEBUG = True
